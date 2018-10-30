fdc7b7b8b5b1c662d8a6201f150181e7606cf561
Gamer4life <uyenwontanswer@gmail.com>
2018-10-30 15:09:07 -0700

    Update startServer.py

--- startServer.py
+++ startServer.py
@@ -1,32 +1,11 @@
-import sys
-import urllib2
-from time import sleep
-import Adafruit_DHT as dht
-# Enter Your API key here
-myAPI = '5QTYDNRHSJ5RESA5' 
-# URL where we will send the data, Don't change it
-baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI 
-def DHT22_data():
-	# Reading from DHT22 and storing the temperature and humidity
-	humi, temp = dht.read_retry(dht.DHT22, 23) 
-	return humi, temp
-while True:
-	try:
-		humi, temp = DHT22_data()
-		# If Reading is valid
-		if isinstance(humi, float) and isinstance(temp, float):
-			# Formatting to two decimal places
-			humi = '%.2f' % humi 					   
-			temp = '%.2f' % temp
-			
-			# Sending the data to thingspeak
-			conn = urllib2.urlopen(baseURL + '&field1=%s&field2=%s' % (temp, humi))
-			print conn.read()
-			# Closing the connection
-			conn.close()
-		else:
-			print 'Error'
-		# DHT22 requires 2 seconds to give a reading, so make sure to add delay of above 2 seconds.
-		sleep(20)
-	except:
-		break
+from flask import Flask, request, jsonify
+ 
+app = Flask(__name__)
+@app.route('/pi', methods=['POST'])
+def pi():
+    pi_data = request.json
+    print(f'Values: {pi_data}') #--> Value on server {'temp': 100, 'temp_1': 150}
+    return jsonify(pi_data)
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
